694c3d9462487fd466601c237ac24448c0390dd8
Jean-Denis Caron <jd.caron@xyz.zone>
2020-04-23 13:22:13 -0400

    Fix regenerating infinite loop.

--- _plugins/copy-cms-data-file.rb
+++ _plugins/copy-cms-data-file.rb
@@ -3,6 +3,8 @@ require 'fileutils'
 Jekyll::Hooks.register :site, :pre_render do |site|
   source = 'admin/config.yml'
   desination = '_data/cms.yml'
-  Jekyll.logger.info "Jekyll Feed:", "Copying file #{source} to #{desination}"
-  FileUtils.cp(source, desination)
+  if !File.file?(desination) || !FileUtils.identical?(source, desination)
+    Jekyll.logger.info "Jekyll Feed:", "Copying file #{source} to #{desination}"
+    FileUtils.cp(source, desination)
+  end
 end
