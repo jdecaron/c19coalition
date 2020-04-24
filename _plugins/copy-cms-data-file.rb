69d843cadc749ce6151badf3feae12ac9d4b6585
Jean-Denis Caron <jd.caron@xyz.zone>
2020-04-24 19:41:27 -0400

    Creating a symlink for the CMS data instead of having a condition to prevent the infinite loop.

--- _plugins/copy-cms-data-file.rb
+++ _plugins/copy-cms-data-file.rb
@@ -1,10 +1,8 @@
 require 'fileutils'
 
-Jekyll::Hooks.register :site, :after_reset do |site|
-  source = 'admin/config.yml'
+Jekyll::Hooks.register :site, :after_init do |site|
+  source = '../admin/config.yml'
   desination = '_data/cms.yml'
-  if !File.file?(desination) || !FileUtils.identical?(source, desination)
-    Jekyll.logger.info "Jekyll Feed:", "Copying file #{source} to #{desination}"
-    FileUtils.cp(source, desination)
-  end
+  Jekyll.logger.info "Jekyll Feed:", "Creating symlink file #{desination} pointing to #{source}"
+  FileUtils.ln_s(source, desination, :force => true)
 end
