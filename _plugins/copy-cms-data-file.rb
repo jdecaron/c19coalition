34039cd8908e0ab66f186af1b259c4b28e48326f
Jean-Denis Caron <jd.caron@xyz.zone>
2020-04-25 11:23:54 -0400

    Adding more comments to explain why we have a plugin for the CMS config file and what to not do.

--- _plugins/copy-cms-data-file.rb
+++ _plugins/copy-cms-data-file.rb
@@ -1,5 +1,9 @@
 require 'fileutils'
 
+# This plugin is used to expose the admin CMS data structure
+# to the Jekyll site. Templates won't see some metadata
+# from the CMS if we don't copy or link the admin/config.yml file to
+# the _data folder.
 Jekyll::Hooks.register :site, :after_init do |site|
   source = '../admin/config.yml'
   desination = '_data/cms.yml'
